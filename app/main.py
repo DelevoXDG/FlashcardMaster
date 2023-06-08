3c13147c415bbf0651de18cb3455ca18be794402
DelevoXDG <max.on777c@gmail.com>
2023-06-08 15:16:25 +0200

    removed mics comments

--- app/main.py
+++ app/main.py
@@ -5,19 +5,13 @@ from modules.main_window import MainWindow
 from modules.deck_widget import DeckWidget
 from modules.models import Base, engine
 
-# Create the database tables (if they don't exist)
-# Base.metadata.create_all(engine)
-
 
 def main():
     app = QApplication(sys.argv)
-    # Create the application
 
-    # Create the main window
     window = MainWindow()
     window.show()
 
-    # Start the application event loop
     sys.exit(app.exec())
 
 
