9d3696c3a8df1d2f2fa5ad76c1aab46f02f68ace
DelevoXDG <max.on777c@gmail.com>
2023-06-11 15:36:05 +0200

    Fixed answer widget display, misc changes to interface

--- app/modules/answer_widget.py
+++ app/modules/answer_widget.py
@@ -1,17 +1,13 @@
-from PyQt6.QtWidgets import (
-    QWidget,  
-    QLabel,  
-    QVBoxLayout,
-    QApplication,
-    QSizePolicy
-)
+from PyQt6.QtWidgets import QWidget, QLabel, QVBoxLayout, QApplication, QSizePolicy
 from PyQt6.QtCore import Qt
 import json
+
 
 class AnswerWidget(QWidget):
     def __init__(self, answer, parent=None):
         super().__init__(parent=parent)
         self.answer = answer
+        self.reveal_answer = False
         self.setup_ui()
 
     def setup_ui(self):
@@ -20,28 +16,34 @@ class AnswerWidget(QWidget):
     def flip(self):
         pass
 
+
 class TextAnswerWidget(AnswerWidget):
     def __init__(self, answer, parent=None):
+        self.hidden_answer_text = "What's you answer?"
+
         super().__init__(answer=answer, parent=parent)
-    
+
     def setup_ui(self):
         layout = QVBoxLayout()
 
-        self.text_answer_label = QLabel("-")
+        self.text_answer_label = QLabel(self.hidden_answer_text)
         layout.addWidget(self.text_answer_label)
 
         self.setLayout(layout)
-    
+
     def flip(self):
-        if self.text_answer_label.text() == "-":
+        self.reveal_answer = not self.reveal_answer
+
+        if self.reveal_answer:
             self.text_answer_label.setText(self.answer)
         else:
-            self.text_answer_label.setText("-")
+            self.text_answer_label.setText(self.hidden_answer_text)
+
 
 class TrueFalseAnswerWidget(AnswerWidget):
     def __init__(self, answer, parent=None):
         super().__init__(answer=answer, parent=parent)
-    
+
     def setup_ui(self):
         layout = QVBoxLayout()
 
@@ -52,22 +54,23 @@ class TrueFalseAnswerWidget(AnswerWidget):
         layout.addWidget(self.answer_false_label)
 
         self.setLayout(layout)
-    
+
     def flip(self):
         if self.answer == "0":
             self.answer_true_label.setVisible(not self.answer_true_label.isVisible())
         else:
             self.answer_false_label.setVisible(not self.answer_false_label.isVisible())
 
+
 class MultipleChoiceAnswer(AnswerWidget):
     def __init__(self, answer, parent=None):
         super().__init__(answer=answer, parent=parent)
-    
+
     def setup_ui(self):
         layout = QVBoxLayout()
 
         self.possibleAnswers = json.loads(self.answer)
-        
+
         self.possible_answer_labels = []
         for key in self.possibleAnswers.keys():
             possible_answer_label = QLabel(key)
@@ -75,15 +78,17 @@ class MultipleChoiceAnswer(AnswerWidget):
             layout.addWidget(possible_answer_label)
 
         self.setLayout(layout)
-    
+
     def flip(self):
         for label in self.possible_answer_labels:
             if self.possibleAnswers[label.text()] == 0:
                 label.setVisible(not label.isVisible())
 
-#Do testów
+
+# Do testów
 if __name__ == "__main__":
     import sys
+
     app = QApplication(sys.argv)
     widget = TextAnswerWidget("text")
     # widget = TrueFalseAnswerWidget("0")
--- app/modules/flashcard_widget.py
+++ app/modules/flashcard_widget.py
@@ -1,42 +1,60 @@
+import os
 import typing
 from PyQt6.QtWidgets import (
-    QApplication, 
+    QApplication,
     QWidget,
     QPushButton,
-    QSizePolicy
+    QSizePolicy,
+    QVBoxLayout,
 )
 from PyQt6.uic import loadUi
 from answer_widget_factory import AnswerWidgetFactory
+
 
 class FlashcardWidget(QWidget):
     def __init__(self, flashcard, parent=None):
         super().__init__(parent)
         self.flashcard = flashcard
         self.load_ui()
+
     def load_ui(self):
-        ui_file = "C:\\Studia\\Inżynieria oprogramowania\\Projekt\\io_project\\app\\modules\\ui\\flashcard_widget.ui"
-        loadUi(ui_file, self)
-        layout = self.layout()
-        answer_widget_factory = AnswerWidgetFactory.getCorrectAnswerWidgetFactory(self.flashcard)
-        answer_widget = answer_widget_factory.create(self.flashcard)
-        answer_widget.setParent(self.answer_widget)
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        ui_path = os.path.join(current_dir, "ui", "flashcard_widget.ui")
+
+        loadUi(ui_path, self)
+        answer_widget_factory = AnswerWidgetFactory.getCorrectAnswerWidgetFactory(
+            self.flashcard
+        )
+        answer_widget = answer_widget_factory.create(self.flashcard, parent=self)
+        # answer_widget.setParent(self.answer_widget)
+
+        placeholder: QWidget = self.answer_widget
+        layout: QVBoxLayout = self.layout
+        layout.replaceWidget(placeholder, answer_widget)
+
+        # self.answer_widget = answer_widget
         self.flip_button.clicked.connect(answer_widget.flip)
         # TrueFalseAnswerWidget("0", self.answer_widget)
 
-#Do testów
+
+# Do testów
 if __name__ == "__main__":
-    class Flashcard():
+
+    class Flashcard:
         def __init__(self, question, answer, card_type) -> None:
             self.question = question
             self.answer = answer
             self.card_type = card_type
 
     import sys
+
     app = QApplication(sys.argv)
 
     text_flashcard = Flashcard("pytanie text", "odpowiedz text", 0)
     true_false_flashcard = Flashcard("pytanie true false", "0", 1)
-    multiple_choice_flashcard = Flashcard("pytanie multiple choice", '{"name":0, "age":0, "car":1}', 2)
+    multiple_choice_flashcard = Flashcard(
+        "pytanie multiple choice", '{"name":0, "age":0, "car":1}', 2
+    )
 
     widget = FlashcardWidget(text_flashcard)
     # widget = FlashcardWidget(true_false_flashcard)
