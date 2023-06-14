de8f1553dce601c998205f257fc6568b64e48866
Herman0802 <germanlazar02@gmail.com>
2023-06-14 06:26:39 +0200

    Improving all widgets, adding a new widget and creating partial functionality for it

--- app/modules/playlist.py
+++ app/modules/playlist.py
@@ -1,14 +1,31 @@
 from collections import deque
 
-from database_models import get_scoped_session
-
+from . import (
+    Deck,
+    Flashcard,
+    get_scoped_session,
+)
 
 class Playlist:
-    def __init__(self, deck_ids):
+    difficulties = {
+        "Easy": [1, 2, 3],
+        "Medium": [2, 1, 3],
+        "Hard": [3, 2, 1],
+    }
+
+    def __init__(self, deck_ids, difficulty=None, study_type=None):
+        # Warto wnieść pewność na temat study_type
         self.flashcard_ids = deque()
         session = get_scoped_session()
-        # get Flashcard sqlalchemy ojbects from db
-        # self.cur_card
+
+        flashcards = session.query(Flashcard).filter(Flashcard.Deck_id.in_(deck_ids)).all()
+
+        if difficulty in self.difficulties:
+            order = self.difficulties[difficulty]
+            flashcards.sort(key=lambda x: order.index(x.difficulty))
+
+        self.flashcard_ids.extend(flashcard.id for flashcard in flashcards)
+        self.cur_card = None
 
     def next(self):
         self.cur_card = self.flashcard_ids.popleft()
--- app/modules/playlist_widget.py
+++ app/modules/playlist_widget.py
@@ -1,3 +1,5 @@
+import os
+
 from PyQt6.QtWidgets import (
     QMainWindow,
     QVBoxLayout,
@@ -9,9 +11,48 @@ from PyQt6.QtWidgets import (
     QPushButton,
     QLineEdit,
     QComboBox,
+    QDialog
 )
 
+from PyQt6 import uic
 
-class PlaylistWidget(QWidget):
-    def __init__(self, decks, parent=None):
+from . import (
+    Deck,
+    Flashcard,
+    FlashcardTableModel,
+    get_scoped_session,
+)
+
+from .playlist import Playlist
+
+class PlaylistWidget(QDialog):
+    def __init__(self, deck_ids, parent=None):
         super().__init__(parent)
+        self.deck_ids = deck_ids
+        self.load_ui()
+
+    def load_ui(self):
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        ui_path = os.path.join(current_dir, "ui", "playlist_dialog.ui")
+        uic.loadUi(ui_path, self)
+
+        self.difficulty_choice: QComboBox = self.difficulty_choice
+        self.study_type_choice: QComboBox = self.study_type_choice
+        self.flip_mode_button: QPushButton = self.flip_mode_button
+        self.flip_mode_button.setAutoDefault(False)
+        self.flip_mode_button.clicked.connect(self.start_study_session)
+        self.quiz_mode_button: QPushButton = self.quiz_mode_button
+        self.quiz_mode_button.setAutoDefault(False)
+        self.quiz_mode_button.setEnabled(False)
+
+    def get_selected_difficulty(self):
+        return self.difficulty_choice.currentText()
+
+    def get_selected_study_type(self):
+        return self.study_type_choice.currentText()
+
+    def start_study_session(self):
+        difficulty = self.get_selected_difficulty()
+        study_type = self.get_selected_study_type()
+        playlist = Playlist(self.deck_ids, difficulty, study_type)
+        # Dalej połączmy ze study_widget
